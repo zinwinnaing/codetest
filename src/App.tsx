6a61b4e48266582116a3eed7696b784ee1b9c3d1
zinwinnaing <zinwinnaing1996820@gmail.com>
2024-06-22 15:16:04 +0630

    add weather need geolocation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.tsx
+import React, { useState, useEffect } from 'react';
+import { Weather, Forecast } from './types';
+import { getCurrentWeather, getWeatherForecast } from './service/weatherService';
+import CitySelector from './component/CitySelector';
+import WeatherCard from './component/WeatherCard';
+import WeatherForecast from './component/WeatherForecast';
 
-function App() {
+const App: React.FC = () => {
+  const [city, setCity] = useState({ label: 'Yangon', value: 'Yangon', latitude: 16.8409, longitude: 96.1735 });
+  const [weather, setWeather] = useState<Weather | null>(null);
+  const [forecasts, setForecasts] = useState<Forecast[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const currentWeather = await getCurrentWeather(city);
+        setWeather(currentWeather);
+        const weatherForecast = await getWeatherForecast(city);
+        setForecasts(weatherForecast);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+        setError('Failed to fetch weather data');
+      }
+    };
+
+    fetchWeather();
+  }, [city]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Weather App</h1>
+      
+ 
+      <CitySelector onCityChange={setCity} />
+      {error && <p className="error">{error}</p>}
+      {weather && <WeatherCard weather={weather} />}
+      <WeatherForecast forecasts={forecasts} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data.tsx
@@ -0,0 +1,11 @@
+export const cityArray = [
+      { label: 'Yangon', value: 'Yangon', latitude: 16.8409, longitude: 96.1735 },
+{ label: 'Mandalay', value: 'Mandalay', latitude: 21.9750, longitude: 96.0840 },
+{ label: 'Naypyidaw', value: 'Naypyidaw', latitude: 19.7633, longitude: 96.0785 },
+{ label: 'Mawlamyine', value: 'Mawlamyine', latitude: 16.4919, longitude: 97.6259 },
+{ label: 'Bago', value: 'Bago', latitude: 17.3361, longitude: 96.4797 },
+{ label: 'Pathein', value: 'Pathein', latitude: 16.7747, longitude: 94.7325 },
+{ label: 'Monywa', value: 'Monywa', latitude: 22.1169, longitude: 95.1373 },
+{ label: 'Meiktila', value: 'Meiktila', latitude: 20.8770, longitude: 95.8580 }
+
+    ];
